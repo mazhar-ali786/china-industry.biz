8c6061ddc0611f1906543e97ec383b0791504d13
Mazhar Ali <mazhar.ali@alchemative.com>
2017-11-03 21:40:47 +0500

    Initial commit

---
+++ china-industry/webapp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿#region Using
+
+using System.Web.Mvc;
+
+#endregion
+
+namespace SmartAdminMvc.Controllers{
+    public class HomeController : Controller
+    {
+        // GET: home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
